7b784d6308ce7b30aed8b015804c83e4c2c99ff4
PakornT <pakorn.ta@gmail.com>
2015-05-24 15:06:40 +0700

    First Commit

---
+++ BWD/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BWD
+//
+//  Created by Pakorn Tangtuengtin on 5/21/15.
+//  Copyright (c) 2015 Pakorn Tangtuengtin. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
